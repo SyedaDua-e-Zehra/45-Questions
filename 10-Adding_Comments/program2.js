11ccf6394f54fcfed05f88a6598fde4938b53c73
SyedaDua-e-Zehra <159719104+SyedaDua-e-Zehra@users.noreply.github.com>
2024-03-27 05:51:14 +0500

    Add files via upload

---
+++ 10-Adding_Comments/program2.js
@@ -0,0 +1,8 @@
+"use strict";
+// Adding Comments: Choose two of the programs you’ve written, and add at least one 
+// comment to each. If you don’t have anything specific to write because your programs are too 
+// simple at this point, just add your name and the current date at the top 
+// of each program file. Then write one sentence describing what the program does.
+// program is written by :Dua
+// Date : 13-2-2024
+console.log(5 * 4);
